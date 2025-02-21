dc912e309aea390808f1aa3ff631c78e0e8d504d
Nathan Brunnett <natebrunnett@gmail.com>
2025-02-21 10:08:59 +0100

    remote Patterns

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: `${process.env.NEXT_PUBLIC_HOSTNAME}`,
-      //   pathname: '/assets/**',
-      // },
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '4040',
+        protocol: 'https',
+        hostname: `${process.env.NEXT_PUBLIC_HOSTNAME}`,
         pathname: '/assets/**',
       },
+      // {
+      //   protocol: 'http',
+      //   hostname: 'localhost',
+      //   port: '4040',
+      //   pathname: '/assets/**',
+      // },
     ],
   },
   eslint: {
