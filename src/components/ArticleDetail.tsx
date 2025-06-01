ac76beb10571b1caf9c6f169bbfe495b1b659efc
Nathaniel <87023327+natbrunt@users.noreply.github.com>
2025-06-01 16:48:56 +0200

    Update ArticleDetail.tsx

--- src/components/ArticleDetail.tsx
+++ src/components/ArticleDetail.tsx
@@ -6,50 +6,44 @@ import Image from "next/image"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import ReactMarkdown from 'react-markdown'
 import markDownComponents from "./markDownComponents"
-/*
-Need an edit mode, it's difficult to get good quality articles up in one sitting,
-so a GUI based interface for handling edits as admin can speed up the article
-production process
+import { articles, Article } from '@/components/articles';
 
-*/
 
-interface Article {
-  id: number
-  title: string
-  description: string
-  date: string
-  image: string
-  content: string
-  preview: string
-}
+// interface Article {
+//   id: number
+//   title: string
+//   description: string
+//   date: string
+//   image: string
+//   content: string
+//   preview: string
+// }
 
 
 
 export function ArticleDetail({ id }: { id: string }) {
-  const [article, setArticle] = useState<Article | null>(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState<string | null>(null)
-  const URL = process.env.NEXT_PUBLIC_SERVER_URL;
+  // const [article, setArticle] = useState<Article | null>(null)
+  // const [loading, setLoading] = useState(true)
+  // const [error, setError] = useState<string | null>(null)
+  // const URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
-  
+  // useEffect(() => {
+  //   const fetchArticle = async () => {
+  //     try {
+  //       const response = await axios.get<Article>(`${URL}tnsv-blog/getArticle/${id}`)
+  //       setArticle(response.data)
+  //     } catch (err) {
+  //       setError("Failed to fetch article.")
+  //     } finally {
+  //       setLoading(false)
+  //     }
+  //   }
 
-  useEffect(() => {
-    const fetchArticle = async () => {
-      try {
-        const response = await axios.get<Article>(`${URL}tnsv-blog/getArticle/${id}`)
-        setArticle(response.data)
-      } catch (err) {
-        setError("Failed to fetch article.")
-      } finally {
-        setLoading(false)
-      }
-    }
+  //   fetchArticle()
+  // }, [id])
 
-    fetchArticle()
-  }, [id])
-
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error}</p>
+  // if (loading) return <p>Loading...</p>
+  // if (error) return <p>Error: {error}</p>
   if (!article) return <p>Article not found.</p>
 
   return (
@@ -60,7 +54,7 @@ export function ArticleDetail({ id }: { id: string }) {
       </CardHeader>
       <div className="relative h-64">
         {article.image ? 
-          <Image src={`${URL}${article.image}`} alt={article.title} layout="fill" objectFit="cover" />
+          <Image src={`$${article.image}`} alt={article.title} layout="fill" objectFit="cover" />
         :
           <Image src={"https://kzml378iqm9efp7rc6zu.lite.vusercontent.net/placeholder.svg"} alt={article.title} layout="fill" objectFit="cover" />
         }    
